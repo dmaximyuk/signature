0eeb10f347d4798c361410f98625b34c8d8c0678
Dmitry Maximyuk <d_maksimyk@vk.com>
2025-09-03 06:00:52 +0500

    refactoring: remove unused

--- src/frontend.ts
+++
@@ -1,5 +0,0 @@
-// Фронтенд версия - все функции (encode + decode для тестирования)
-export { encode, decode } from "./telegram/index.js";
-export { decode as vkDecode } from "./vkontakte/index.js";
-export { type Response as TelegramResponse } from "./telegram/decode/decode.js";
-export { type Response as VKResponse } from "./vkontakte/decode/decode.js";
